907cc20aab7963becb3c2bbddb071bb19abbc13a
Bruno Ribeiro <brunoribca@gmail.com>
2021-02-21 14:41:24 +0100

    set local image namem not versioned

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -e
 
-VERSION=$(./gradlew properties -q | grep "version:" | awk '{print $2}')
-TARGET_IMAGE_VERSIONED="${DOCKER_REPO}:${VERSION}"
-
 ./gradlew build
-./gradlew bootBuildImage --imageName="${TARGET_IMAGE_VERSIONED}"
+./gradlew bootBuildImage --imageName="${DOCKER_REPO}"
